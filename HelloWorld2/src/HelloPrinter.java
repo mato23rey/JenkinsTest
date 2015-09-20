b3e851893e28d725bb6b34cb605e1889432d7ec2
mato23rey <mato23rey@gmail.com>
2015-09-20 20:11:48 -0300

    checkout

--- HelloWorld2/src/HelloPrinter.java
+++ HelloWorld2/src/HelloPrinter.java
@@ -4,6 +4,7 @@ public class HelloPrinter {
 	
 	public boolean printHello (String text){
 		
+		//Comentario para tito
 		if(text.compareTo("Hello")==0){
 			System.out.println("Hello Word");
 			return true;
